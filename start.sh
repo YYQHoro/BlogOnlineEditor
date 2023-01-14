ab3765194b6dcb11f783cae92ddf4ef570c3a690
YangYuQing <hiyyq@qq.com>
2023-01-14 21:16:05 +0800

    修复日志路径错误的问题

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 CUR_PATH=$(dirname $(realpath "$0"))
-nohup python3 $CUR_PATH/app.py >run.log 2>&1 & echo $! > $CUR_PATH/pid
+nohup python3 $CUR_PATH/app.py >$CUR_PATH/run.log 2>&1 & echo $! > $CUR_PATH/pid
